d99d44c5403ec1584629919f3676e54ce7345f0c
zhoubo8308 <zhoubo8308@163.com>
2016-08-24 14:21:06 +0800

    add

--- method_string_replace.js
+++ method_string_replace.js
@@ -1,4 +1,40 @@
-﻿var regep = /:(\w+)/g;
+﻿//string.replace(searchValue,replaceValue);
+/**
+*The search value can be a string or regular expression object. If it is a string,only the first occurance of the value is replaced.
+*/
+
+var result1='mother-in-law'.replace('-','_');
+var result2='mother-in-law'.replace(/-/,'_');
+var result3='mother-in-law'.replace(/-/g,'_');
+console.log('result1 = ' + result1);
+console.log('result2 = ' + result2);
+console.log('result3 = ' + result3);
+
+/**
+* If the replaceValue is a string, the character $ has special meaning.
+*p = 555-666-1212
+*p1 = $666-1212
+*p2 = (555)666-1212
+*p3 = 666-1212
+*p4 = 666-1212666-1212
+*/
+var oldAreaCode = /\((\d{3})\)/g;
+var p = '(555)666-1212'.replace(oldAreaCode,'$1-');//replace with caputure group text "555"
+var p1 = '(555)666-1212'.replace(oldAreaCode,'$$');//replace with '$'
+var p2 = '(555)666-1212'.replace(oldAreaCode,'$&');//replace with the matched text "(555)"
+var p3 = '(555)666-1212'.replace(oldAreaCode,'$`');//replace with the text preceding the match ""
+var p4 = '(555)666-1212'.replace(oldAreaCode,"$'");//replace with the text following the match "666-1212"
+console.log('p = ' + p);
+console.log('p1 = ' + p1);
+console.log('p2 = ' + p2);
+console.log('p3 = ' + p3);
+console.log('p4 = ' + p4);
+
+
+/**
+*replaceValue is a function
+*/
+var regep = /:(\w+)/g;
 var testStr = ':method :url'
 var req = {method:'get',url:'www.lottery-bob.com'}
 var str = testStr.replace(regep,function(match,property){
