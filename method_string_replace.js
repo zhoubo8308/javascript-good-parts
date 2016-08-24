2064c9f127a1e6fffcf38ee52b59968896965499
U-SCHENKER-ASIA\bzou <bzou@SSC1-W303.schenker-asia.com>
2016-08-24 13:44:23 +0800

    add string replace

---
+++ method_string_replace.js
@@ -0,0 +1,8 @@
+﻿var regep = /:(\w+)/g;
+var testStr = ':method :url'
+var req = {method:'get',url:'www.lottery-bob.com'}
+var str = testStr.replace(regep,function(match,property){
+	console.log(match+'	'+property);
+	return req[property];
+});
+console.log(str);
